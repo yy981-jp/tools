9db2b9ec8271a2df4358a18f22b68440ff5ce3bd
yy981-jp <134594870+yy981-jp@users.noreply.github.com>
2025-06-21 23:27:18 +0900

    randomSeed() 追加

--- random.h
+++ random.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <random>
+#include <chrono>
+#include <sstream>
 
 
 inline int randomNum(const int& low, const int& up) {
@@ -7,4 +9,13 @@ inline int randomNum(const int& low, const int& up) {
 	std::mt19937 gen(rd());
 	std::uniform_int_distribution<int> distribution(low, up);
 	return distribution(gen);
+}
+
+inline std::string randomSeed() {
+	std::random_device rd;
+	std::mt19937_64 eng(rd());
+	auto now = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+	std::stringstream ss;
+	ss << eng() << now;
+	return ss.str();
 }
