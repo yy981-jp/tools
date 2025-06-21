a330b9d16786ab293d63307350c339d14b904f52
yy981-jp <134594870+yy981-jp@users.noreply.github.com>
2025-06-21 19:58:40 +0900

    return inline化

--- return.h
+++ return.h
@@ -3,13 +3,13 @@
 #include <string>
 #include <cstdlib>
 
-void return_u(const std::string i, bool noText = false) {
+inline void return_u(const std::string i, bool noText = false) {
 	if (!noText) std::cout << "Usage: " << i << std::endl;
 	else std::cout << i << std::endl;
 	exit(0);
 }
 
-void return_e(const std::string i, int returnCode = 1) {
+inline void return_e(const std::string i, int returnCode = 1) {
 	std::cerr << "ERROR: " << i << std::endl;
 	exit(returnCode);
 }
